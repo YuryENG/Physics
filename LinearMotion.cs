8c87fb13d7f76d4068d11547b74cbb9e24081dce
YuryENG <31512684+YuryENG@users.noreply.github.com>
2017-09-01 00:03:03 -0400

    Update LinearMotion.cs
    Added:
     //time that takes to travel the distance d , acceleration a, to the maximum speed v
     // isosceles trapezoid linear motion with equal acceleration and deceleration 
    Full description is here: https://github.com/YuryENG/Physics/wiki/Isosceles-trapezoid-linear-motion

--- LinearMotion.cs
+++ LinearMotion.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,7 @@ namespace Kinematics
         //d - distance
         //t - time
         //v - speed
-        //a - acceliration
+        //a - acceleration
         //Vo - initial speed
 
         // find distance when speed and time is known 
@@ -66,6 +66,37 @@ namespace Kinematics
         public static double TimeByDistanceAndAcceleration(double Acceleration, double Distance)
         {
             return Math.Sqrt(2 * Distance / Acceleration);
+        }
+
+
+        //time that takes to travel the distance d , acceleration a, to the maximum speed v
+        // isosceles trapezoid linear motion with equal acceleration and deceleration 
+
+        public static double TrapezoidMotionTime(double distance, double acceleration, double maximum_speed)
+        {
+            // create short names 
+            double d = distance;
+            double a = acceleration;
+            double v = maximum_speed;
+
+            // time that will take to reach full speed
+
+            double Tfs = v / a;
+
+            // distance that will take to reach full speed
+
+            double Dfs = a * Tfs * Tfs * 0.5;
+
+            if (d > 2*Dfs)
+            {
+                double Tv = (d - 2 * Dfs) / v;
+                return 2 * Tfs + Tv;
+            }
+            else
+            {
+                return 2 * Math.Sqrt(d / a);
+            }
+            
         }
     }
 }
